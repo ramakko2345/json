6d01e7f58a07b2d2bfce7d57cc12413465c96d3c
ramakko2345 <ogp777@gmail.com>
2024-05-21 17:54:54 +0900

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,9 @@
 javascript:(function(){
-  fetch('https://ramakko2345.github.io/json/data.json?' + new Date().getTime())
+  let active = false;
+  const func = (targetNode) => {
+    if (active) return;
+    active = true;
+    fetch('https://ramakko2345.github.io/json/data.json?' + new Date().getTime())
     .then(response => response.json())
     .then(data => {
       const replaceTexts = (replacements, isAspect, options) => {
@@ -14,19 +18,44 @@ javascript:(function(){
           } else if (node.nodeType === 3) {
             replacements.forEach(rep => {
               if (rep.original) {
-                const regExp = new RegExp(isAspect ? `Aspect of ${rep.original}|${rep.original} Aspect` : rep.original, options);
+                const regExp = new RegExp(isAspect ? `Aspect of ${rep.original}|${rep.original} Aspect|of ${rep.original}|${rep.original} of` : rep.original, options);
                 node.nodeValue = node.nodeValue.replace(regExp, rep.new);
               }
             });
           }
         };
-        walk(document.body);
+        walk(targetNode);
       };
 
-      replaceTexts(data.aspects, true, 'gi');
+      const aspects = data.aspects;
+      aspects.sort((a, b) => (b.original?.length || 0) - (a.original?.length || 0));
+      replaceTexts(aspects, true, 'gi');
       const reps = data.replacements;
       reps.sort((a, b) => (b.original?.length || 0) - (a.original?.length || 0));
       replaceTexts(reps, false, 'g');
+      replaceTexts(aspects, false, 'gi');
     })
     .catch(error => console.error('Error loading JSON:', error));
+    active = false;
+  };
+
+  func(document.body);
+
+  if (document.body.getAttribute('obflg') != 't') {
+    const callback = function(mutationsList, observer) {
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          mutation.addedNodes.forEach(node => {
+            if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'DIV') {
+              func(node);
+            }
+          });
+        }
+      }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(document.body, {childList: true, subtree: true});
+    
+    document.body.setAttribute('obflg', 't');
+  }
 })();
