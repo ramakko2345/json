2e5abe0853ea1167b7cb6f9a4fa9e39390fd501e
ramakko2345 <ogp777@gmail.com>
2024-05-22 16:42:53 +0900

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -11,9 +11,15 @@ javascript:(function(){
         }
       } else if (node.nodeType === 3) {
         replacements.forEach(rep => {
-          if (rep.original && rep.new && rep.new.length > 0) {
-            const regExp = new RegExp(isAspect ? `Aspect of ${rep.original}|${rep.original} Aspect|of ${rep.original}|${rep.original} of` : rep.original, options);
-            node.nodeValue = node.nodeValue.replace(regExp, rep.new);
+          if (rep.original && rep.new && rep.new.length > 0 && !rep.error) {
+            const regex = isAspect ? `Aspect of ${rep.original}|${rep.original} Aspect|of ${rep.original}|${rep.original} of` : rep.original;
+            try {
+              const regExp = new RegExp(regex, options);
+              node.nodeValue = node.nodeValue.replace(regExp, rep.new);
+            } catch(e) {
+              console.error('Error replace :', e, regex, rep);
+              rep.error = true;
+            }
           }
         });
       }
@@ -28,12 +34,31 @@ javascript:(function(){
     replaceNodes(targetNode, json.aspects, false, 'g');
   };
   
+
   fetch(url + '?' + new Date().getTime())
   .then(response => response.json())
   .then(data => {
     data.aspects.sort((a, b) => (b.original?.length || 0) - (a.original?.length || 0));
     data.replacements.sort((a, b) => (b.original?.length || 0) - (a.original?.length || 0));
-    json = data;
+    let map = new Map();
+    let aspects = [];
+    const dup = (replacements, lists) => {
+      replacements.forEach(rep => {
+        if (rep.original && rep.new && rep.new.length > 0) {
+          if (map.has(rep.original)) {
+            console.log('exists:', rep.original, rep.new, map.get(rep.original));
+          } else {
+            lists.push(rep);
+            map.set(rep.original, rep.new);
+          }
+        }
+      });
+    };
+    dup(data.aspects, aspects);
+    map = new Map();
+    let replacements = [];
+    dup(data.replacements, replacements);
+    json = {aspects: aspects, replacements: replacements};
     replaceTexts(document.body);
 
     if (document.body.getAttribute('obflg') != 't') {
@@ -54,4 +79,6 @@ javascript:(function(){
     }
   })
   .catch(error => console.error('Error loading JSON:', error));
+
+
 })();
